61c7b5ca626b2d59863247595e37f8b644b380df
Razvan Goga <razvan.goga@gmail.com>
2017-04-03 19:39:20 +0200

    Add project files.

---
+++ ApplicationInsightsTests/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.ApplicationInsights.Extensibility;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationInsightsTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ikey = "416d0687-c13b-42b8-9cbb-5aa7c257b08f";
+
+            TelemetryConfiguration.Active.InstrumentationKey = ikey;
+            TelemetryConfiguration.Active.TelemetryChannel.DeveloperMode = true;
+
+            string key = "ConsoleTest";
+
+            Trace.TraceError($"{key} - error");
+            Trace.TraceInformation($"{key} - info");
+            Trace.TraceWarning($"{key} - warning");
+            Trace.Write($"{key} - write", "my category");
+
+            //TraceSource ts = new TraceSource("My Verbose Debugger") { Switch = { Level = SourceLevels.All } };
+            //ts.TraceData(TraceEventType.Verbose, 0, $"{key} - verbose");
+            //ts.Flush();
+
+            Console.WriteLine("Done...");
+            Console.ReadKey();
+        }
+    }
+}
